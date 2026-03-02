5e5992cd353046da1c8e8faf2bfd35176e980479
Sid Bentifraouine <contact@sidali.dev>
2026-03-02 14:06:31 +0100

    Fix syntax error in main route handler

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 // Route principale
-app.get('/', (req, res) => {asdfadfsadf
+app.get('/', (req, res) => {
   res.json({ message: 'Bienvenue sur l\'API DevOps Demo' })
 })
 
